c8969df0606170fa8db06ab872d5baa9acce4681
Victor Daniel Guzman Perez <vdgp_12@hotmail.com>
2021-05-11 20:15:10 -0500

    primer commit

---
+++ src/barberodormilon/BarberoDormilon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barberodormilon;
+
+/**
+ *
+ * @author vdgp_
+ */
+public class BarberoDormilon {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Interface inter= new Interface();
+        inter.show();
+    }
+    
+}
---
+++ src/barberodormilon/hilo.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package barberodormilon;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author vdgp_
+ */
+public class hilo extends Thread{
+    int tiempo;
+    public hilo (int tiempo_){
+        tiempo=tiempo_;
+    }
+    @Override
+    public void run(){
+      
+    }
+}
